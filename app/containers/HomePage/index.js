b022e43bfb07718e519b7f7ccd093a9e282a8ae6
Eric Betts <bettse@fastmail.fm>
2016-10-29 14:39:13 -0700

    Iterate through list of blinks

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -13,18 +13,42 @@
 import React from 'react';
 import { BlockPicker } from 'react-color';
 
+const blinks = [
+  {
+    "username": "bettse",
+    "displayname": "Eric",
+    "iftttkey": "1661ebd420004adc"
+  }
+]
+
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  state = {
-    background: '#0ff',
-    colors: ['#fff', '#000'],
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      background: '#0ff',
+      colors: ['#fff', '#000'],
+      blinks: blinks
+    };
+  }
 
   handleChangeComplete = (color) => {
     console.log(color.hex);
     this.setState({ background: color.hex });
   };
+
   render() {
-    return <BlockPicker color={ this.state.background } onChangeComplete={ this.handleChangeComplete } />;
+    const {blinks, background} = this.state;
+    return (
+      <div>
+      <h1>Control team blink1s</h1>
+      {blinks.map((blink, index) => (
+        <div key={index} className={blink.username}>
+          <h2>{blink.displayname}</h2>
+          <BlockPicker color={ background } onChangeComplete={ this.handleChangeComplete } iftttkey={blink.iftttkey} />
+        </div>
+      ))}
+      </div>
+    )
   }
 }
 
