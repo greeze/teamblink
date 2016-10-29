e1052bb091b13f5ce75f32c00062d470dcce5748
Eric Betts <bettse@fastmail.fm>
2016-10-29 11:45:57 -0700

    Displays Block color picker

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,25 +1,30 @@
 /*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
+* HomePage
+*
+* This is the first thing users see of our App, at the '/' route
+*
+* NOTE: while this component should technically be a stateless functional
+* component (SFC), hot reloading does not currently support SFCs. If hot
+* reloading is not a necessity for you then you can refactor it and remove
+* the linting exception.
+*/
+
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { BlockPicker } from 'react-color';
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  state = {
+    background: '#0ff',
+    colors: ['#fff', '#000'],
+  };
 
+  handleChangeComplete = (color) => {
+    console.log(color.hex);
+    this.setState({ background: color.hex });
+  };
   render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
+    return <BlockPicker color={ this.state.background } onChangeComplete={ this.handleChangeComplete } />;
   }
 }
+
