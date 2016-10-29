43947ac1647b8e892346c5afc99595cc62575649
Eric Betts <bettse@fastmail.fm>
2016-10-29 15:27:59 -0700

    Select user, click color, sends

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,14 +12,8 @@
 
 import React from 'react';
 import { BlockPicker } from 'react-color';
-
-const blinks = [
-  {
-    "username": "bettse",
-    "displayname": "Eric",
-    "iftttkey": "1661ebd420004adc"
-  }
-]
+const blinks_gist = 'https://gist.githubusercontent.com/bettse/2d97bd6c10ca84eeb4652b4d4ac18374/raw/38a159b28296596e927eaa594714fdde1209f495/blinks.json'
+const update_base= 'https://stacks.stackery.io/23628740815157/blink'
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -27,26 +21,53 @@ export default class HomePage extends React.Component { // eslint-disable-line r
     this.state = {
       background: '#0ff',
       colors: ['#fff', '#000'],
-      blinks: blinks
+      blinks: [],
+      selectedBlink: {}
     };
   }
 
   handleChangeComplete = (color) => {
-    console.log(color.hex);
+    const { selectedBlink } = this.state
+    const { iftttkey } = selectedBlink
+    if (iftttkey) {
+      fetch(`${update_base}/${iftttkey}`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          value: color.hex
+        })
+      })
+    }
     this.setState({ background: color.hex });
   };
 
+  handleBlinkSelect = (blink) => {
+    this.setState({ selectedBlink: blink });
+  }
+
+  componentDidMount() {
+    fetch(blinks_gist).then((response) => {
+      return response.json().then((json) => {
+        this.setState({blinks: json, selectedBlink: json[0]});
+      })
+    })
+  }
+
   render() {
-    const {blinks, background} = this.state;
+    const {blinks, selectedBlink, background} = this.state;
     return (
       <div>
-      <h1>Control team blink1s</h1>
-      {blinks.map((blink, index) => (
-        <div key={index} className={blink.username}>
-          <h2>{blink.displayname}</h2>
-          <BlockPicker color={ background } onChangeComplete={ this.handleChangeComplete } iftttkey={blink.iftttkey} />
-        </div>
-      ))}
+        <h1>Control team blink1s</h1>
+        <select name="select">
+          {blinks.map((blink, index) => (
+            <option key={index} value={blink.username} onChange={ this.handleBlinkSelect.bind(this, blink) }>{blink.displayname}</option>
+          ))}
+        </select>
+        <h2>{selectedBlink.displayname}</h2>
+        <BlockPicker color={ background } onChangeComplete={ this.handleChangeComplete } />
       </div>
     )
   }
